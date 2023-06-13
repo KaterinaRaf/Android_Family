10355c398a5150fa59b0f5b16e848177725c5b00
Katerina Raf <KateSu1989@gmail.com>
2023-06-13 09:09:57 +0500

    Second stage

--- app/src/main/java/com/example/myfamily/MainActivity.java
+++ app/src/main/java/com/example/myfamily/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myfamily;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickCreateFamily(View view) {
+        TextView text_family = (TextView) findViewById(R.id.text_family);
+        Spinner familyKin = (Spinner) findViewById(R.id.familyKin);
+        Spinner nameKin = (Spinner) findViewById(R.id.nameKin);
+        String result1 = String.valueOf(familyKin.getSelectedItem());
+        String result2 = String.valueOf(nameKin.getSelectedItem());
+        String resultFamily = result1 + " " + result2;
+        text_family.setText(resultFamily);
     }
 }
